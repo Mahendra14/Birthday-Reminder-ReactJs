af284009444602a9e21bd7e84046a22af68610cf
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-03 16:04:39 +0530

    Added populate and clear all to the button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  const [people, setPeople] = useState(data);
+  const [people, setPeople] = useState([]);
+
+  const clearAndRevertButtonFn = () => {
+    console.log(people);
+    if(people.length === 0){
+    setPeople(data);
+    }
+    else{
+        setPeople([]);
+    } 
+  }
   return (<main> 
-    <section class="container">
+    <section className="container">
       <h3>{people.length} birthdays today!</h3>
       <List people = {people}/>
-      <button onClick={() => {setPeople([])}} > Click Me!</button>
+      <button onClick={clearAndRevertButtonFn} > {people.length === 0 ? "Populate" : "Clear All!"}</button>
     </section>
   </main> );
 }
