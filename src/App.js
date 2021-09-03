d14ec773df3dc694bf2469984a74ff34b62f6e2d
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-09-03 15:31:21 +0530

    Added list component to App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (<main> 
+    <section class="container">
+      <h3>0 birthdays today!</h3>
+      <List />
+      <button onClick={() => {console.log("you clicked me!")}} > Click Me!</button>
+    </section>
+  </main> );
 }
-
 export default App;
--- src/List.js
+++ src/List.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const List = () => {
   return (
-    <>
+    <div>
       <h2>list component</h2>
-    </>
+    </div>
   );
 };
 
